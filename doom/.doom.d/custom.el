8299438e9a2490f06eada485e17a869a7eb82aa0
sergiotaerri <sergio2000jr@gmail.com>
2020-12-15 03:45:45 -0300

    add: clojure mode configuration starts!
    lispy-map problem fixed and
    cider repl placement is now to the right

--- doom/.doom.d/custom.el
+++ doom/.doom.d/custom.el
@@ -5,7 +5,9 @@
  ;; If there is more than one, they won't work right.
  '(custom-safe-themes
    '("f2927d7d87e8207fa9a0a003c0f222d45c948845de162c885bf6ad2a255babfd" "4a8d4375d90a7051115db94ed40e9abb2c0766e80e228ecad60e06b3b397acab" default))
- '(safe-local-variable-values '((buffer-file-coding-system . iso-8859-1))))
+ '(safe-local-variable-values
+   '((git-commit-major-mode . git-commit-elisp-text-mode)
+     (buffer-file-coding-system . iso-8859-1))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
