2f47856becb2f9d014cc57fff597023e180d9773
sergiotaerri <sergio2000jr@gmail.com>
2021-01-09 15:56:26 -0300

    add: bullets 'n fonts

--- doom/.doom.d/custom.el
+++ doom/.doom.d/custom.el
@@ -3,11 +3,10 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(ansi-color-names-vector
+   ["#242730" "#D16969" "#579C4C" "#D7BA7D" "#339CDB" "#C586C0" "#85DDFF" "#d4d4d4"])
  '(custom-safe-themes
-   '("f2927d7d87e8207fa9a0a003c0f222d45c948845de162c885bf6ad2a255babfd" "4a8d4375d90a7051115db94ed40e9abb2c0766e80e228ecad60e06b3b397acab" default))
- '(safe-local-variable-values
-   '((git-commit-major-mode . git-commit-elisp-text-mode)
-     (buffer-file-coding-system . iso-8859-1))))
+   '("2f1518e906a8b60fac943d02ad415f1d8b3933a5a7f75e307e6e9a26ef5bf570" "4b72327be4ac1b2121c4ce3c30c21b820ee8364fe47a9673c8532dfe13dcdcee" default)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
